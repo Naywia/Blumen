12b52bc15daf490bb4f4b00ed2dc5c904c4eea93
Sammy <144029136+DrZarag@users.noreply.github.com>
2024-04-11 14:31:34 +0200

    Use Case 4: Tilpas kundeoplysninger

--- ViewModels/CustomerViewModel.cs
+++ ViewModels/CustomerViewModel.cs
@@ -1,14 +1,15 @@
 ﻿using Blumen.Models;
 using Blumen.Persistence;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Blumen.ViewModels
 {
-    internal class CustomerViewModel
+    public class CustomerViewModel : ObservableObject
     {
         private ICommand updateCustomerCommand;
         private CustomerRepo customerRepo = App.CustomerRepo;
-        public Customer Customer { get; set; }
+        private Customer customer;
 
         public ICommand UpdateCustomerCommand
         {
@@ -20,9 +21,84 @@ namespace Blumen.ViewModels
             }
         }
 
+        private Window currentWindow;
+        public CustomerViewModel(Window window, int customerIndex)
+        {
+            currentWindow = window;
+            this.customer = customerRepo.GetItems()[customerIndex];
+
+            Name = customer.Name;
+            Address = customer.Address;
+            PhoneNumber = customer.PhoneNumber;
+            Email = customer.Email;
+            PaymentNumber = customer.PaymentNumber;
+        }
+
         public void UpdateCustomer()
         {
-
+            customerRepo.UpdateItem(customer, new Customer() { Name = this.Name, Address = this.Address, PhoneNumber = this.PhoneNumber, Email = this.Email, PaymentNumber = this.PaymentNumber });
+            currentWindow.Close();
         }
+
+        public PaymentNumberType SelectedPaymentNumberType
+        {
+            get => selectedPaymentNumberType;
+            set
+            {
+                selectedPaymentNumberType = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private PaymentNumberType selectedPaymentNumberType;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string name;
+        public string Address
+        {
+            get => address;
+            set
+            {
+                address = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string address;
+        public long PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                phoneNumber = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private long phoneNumber;
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string email;
+        public long PaymentNumber
+        {
+            get => paymentNumber;
+            set
+            {
+                paymentNumber = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private long paymentNumber;
     }
 }
--- Views/CustomerView.xaml.cs
+++ Views/CustomerView.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Blumen.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Blumen.Views
 {
     /// <summary>
     /// Interaction logic for CustomerView.xaml
     /// </summary>
-    public partial class CustomerView : Page
+    public partial class CustomerView : Window
     {
-        public CustomerView()
+        public CustomerView(int itemIndex)
         {
             InitializeComponent();
+            DataContext = new CustomerViewModel(this, itemIndex);
+        }
+
+        private void CloseUpdateCustomer(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
