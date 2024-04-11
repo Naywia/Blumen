dd7eb1a25e03233b4569b9824b257ad93e27ccf7
Naywia <48431873+Naywia@users.noreply.github.com>
2024-04-11 08:57:43 +0200

    Create RelayCommand.cs

---
+++ Models/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Blumen.Models
+{
+    internal class RelayCommand : ICommand
+    {
+        Action<object> _executeMethod;
+        Func<object, bool> _canExecuteMethod;
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> executeMethod, Func<object, bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod == null || _canExecuteMethod(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod(parameter);
+        }
+    }
+}
