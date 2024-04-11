ccc6b74d71b0fb8f7022d33326c2a0b55b08cd8a
Naywia <48431873+Naywia@users.noreply.github.com>
2024-04-11 08:52:00 +0200

    Make viewmodels from DCD

---
+++ ViewModels/OrderOverviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Blumen.ViewModels
+{
+    public class OrderOverviewViewModel
+    {
+        private OrderRepo orderRepo = App.OrderRepo;
+        public ObservableCollection<Order> Orders { get; set; }
+        public Order Order { get; set; }
+        public ICommand SelectOrderCommand { get; set; }
+    }
+}
