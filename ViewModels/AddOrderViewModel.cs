7e4e661d71e82d5320852827b58c5819d582aa7d
Naywia <48431873+Naywia@users.noreply.github.com>
2024-04-11 16:54:30 +0200

    Add stuff from AddCustomer and edit it fit AddOrder

--- ViewModels/AddOrderViewModel.cs
+++ ViewModels/AddOrderViewModel.cs
@@ -5,12 +5,18 @@ using System.Windows.Input;
 
 namespace Blumen.ViewModels
 {
-    public class AddOrderViewModel
+    public class AddOrderViewModel : ObservableObject
     {
         #region Fields
         private ICommand addOrderCommand;
         private OrderRepo orderRepo = App.OrderRepo;
         private Window currentWindow;
+
+        private int orderNumber;
+        private DateTime orderDate;
+        private string deliveryAddress;
+        private Payment paymentStatus;
+        private string comment;
         #endregion
 
         #region Constructors
@@ -30,12 +36,66 @@ namespace Blumen.ViewModels
                 return addOrderCommand;
             }
         }
+
+        public int OrderNumber
+        {
+            get => orderNumber;
+            set
+            {
+                orderNumber = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public DateTime OrderDate
+        {
+            get => orderDate;
+            set
+            {
+                orderDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string DeliveryAddress
+        {
+            get => deliveryAddress;
+            set
+            {
+                deliveryAddress = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public Payment PaymentStatus
+        {
+            get => paymentStatus;
+            set
+            {
+                paymentStatus = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Comment
+        {
+            get => comment;
+            set
+            {
+                comment = value;
+                NotifyPropertyChanged();
+            }
+        }
         #endregion
 
         #region Methods
         public void AddOrder()
         {
-
+            orderRepo.AddItem(new Order()
+            {
+                OrderNumber = OrderNumber,
+                OrderDate = OrderDate,
+                DeliveryAddress = DeliveryAddress,
+                PaymentStatus = PaymentStatus,
+                Comment = Comment
+            });
+            currentWindow.Close();
         }
         #endregion
     }
