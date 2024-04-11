e41e49cc6a8ea8aed3bd301dcaf02919cc1ebdc1
Naywia <48431873+Naywia@users.noreply.github.com>
2024-04-11 08:44:19 +0200

    Add Everything from DCD

---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blumen.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PaymentNumber { get; set; }
+        public PaymentNumberType PaymentNumberType { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blumen.Models
+{
+    internal class Order
+    {
+    }
+}
