0c1e98d6f3aa201689528023c6ff2ff298b8edae
Rasmus Nørby <52028666+r4sNorby@users.noreply.github.com>
2024-05-08 10:47:43 +0200

    Use Case 15: Indføj service i system
    Co-Authored-By: Mikkel Paulick <58453563+mrpink32@users.noreply.github.com>
    Co-Authored-By: Naywia <48431873+Naywia@users.noreply.github.com>
    Co-Authored-By: Sammy <144029136+DrZarag@users.noreply.github.com>

---
+++ ViewModels/AddServiceViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Blumen.Models;
+using Blumen.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Blumen.ViewModels
+{
+    class AddServiceViewModel : ObservableObject
+    {
+        #region Fields
+        private ServiceRepo serviceRepo = new ServiceRepo();
+        private Window currentWindow;
+        private ICommand addServiceCommand;
+        private string name;
+        private double price;
+        #endregion
+
+        #region Constructors
+        public AddServiceViewModel(Window window)
+        {
+            currentWindow = window;
+        }
+        #endregion
+
+        #region Properties
+        public ICommand AddServiceCommand
+        {
+            get
+            {
+                addServiceCommand ??= new RelayCommand(MethodToRun => AddService());
+                return addServiceCommand;
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public double Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                NotifyPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void AddService()
+        {
+            serviceRepo.AddItem(new Service()
+            {
+                Name = Name,
+                Price = Price,
+            });
+            currentWindow.Close();
+        }
+        #endregion
+    }
+}
---
+++ ViewModels/ServiceOverviewViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Blumen.Models;
+using Blumen.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blumen.ViewModels
+{
+    class ServiceOverviewViewModel
+    {
+        #region Fields
+        private ServiceRepo serviceRepo = new();
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Service> Services { get => serviceRepo.GetItems(); }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
