2f6f3f7f0cc0e8b8fb5a42800db0c9c83a7da66d
Naywia <48431873+Naywia@users.noreply.github.com>
2024-05-12 12:51:20 +0200

    Use Case 3: Find kontaktoplysninger på kunde

---
+++ Extentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blumen
+{
+    public static class Extentions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> original)
+        {
+            return new ObservableCollection<T>(original);
+        }
+    }
+}
